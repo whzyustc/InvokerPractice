f53dbadc17c591240f8e1561cae7a6688f63d0ce
whzyustc <whzyzyz@outlook.com>
2020-05-30 11:32:48 +0800

    liveserver

--- src/component/sidetab.tsx
+++ src/component/sidetab.tsx
@@ -8,7 +8,7 @@ export interface IHelloProps {
 export default class SideTab extends React.Component<IHelloProps>
 {
     render(){
-     return <ul>{`${this.props.compiler} and ${this.props.framework} project`.split(' ').map((item,index)=><li key={index}>{item}</li>)}</ul>
+     return <ul>{`Invoker Practise`.split(' ').map((item,index)=><li key={index}>{item}</li>)}</ul>
     }
 }
 
